21e99d6bfaf411a7f716a14c1201896baf7890af
taul8123 <51949055+taul8123@users.noreply.github.com>
2023-04-12 15:51:04 +0900

    week6

---
+++ Task/week6/SimpleDiary/app/src/main/java/com/cookandroid/simplediary/MainActivity.java
@@ -0,0 +1,88 @@
+package com.cookandroid.simplediary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+
+
+/* *********************
+* 프로젝트명 : Simple Diary
+* 작정자 : 윤석현
+* 작성일 : 2023.04.12
+* 프로젝트 설명 : 간단한 일기장 애플리케이션
+*********************** */
+public class MainActivity extends AppCompatActivity {
+    DatePicker dp;
+    EditText edtDiary;
+    Button btnWrite;
+    String fileName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단 일기장");
+
+        dp = (DatePicker) findViewById(R.id.dataPicker1);
+        edtDiary = (EditText) findViewById(R.id.edtDiary);
+        btnWrite = (Button) findViewById(R.id.btnWrite);
+
+        Calendar cal = Calendar.getInstance();
+        int cYear = cal.get(Calendar.YEAR);
+        int cMonth = cal.get(Calendar.MONTH);
+        int cDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        dp.init(cYear, cMonth, cDay, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                fileName = Integer.toString(year) + "_"
+                        + Integer.toString(monthOfYear) + "_"
+                        + Integer.toString(dayOfMonth) + ".txt";
+                String str = readDiary(fileName);
+                edtDiary.setText(str);
+                btnWrite.setEnabled(true);
+            }
+        });
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str = edtDiary.getText().toString();
+                    outFs.write(str.getBytes());
+                    outFs.close();
+                    Toast.makeText(getApplicationContext(), fileName+"이 저장됨", Toast.LENGTH_SHORT).show();
+                } catch (IOException e){
+
+                }
+            }
+        });
+    }
+    String readDiary(String fName){
+        String diaryStr = null;
+        FileInputStream inFs;
+        try{
+            inFs = openFileInput(fName);
+            byte[] txt = new byte[500];
+            inFs.read(txt);
+            inFs.close();
+            diaryStr = (new String(txt)).trim();
+            btnWrite.setText("수정하기");
+        } catch (IOException e){
+            edtDiary.setHint("일기 없음");
+            btnWrite.setText("새로 저장");
+        }
+        return diaryStr;
+    }
+}
